bfad1cb3bd26fbed4385742ff4c8423ba5509a42
Ahmad Fajar <ahmad.fajar234@yahoo.com>
2022-09-06 08:06:22 +0700

    Update components: BsCheckbox, BsRadio scss file

--- src/components/Radio/mixins/radioApi.ts
+++ src/components/Radio/mixins/radioApi.ts
@@ -69,7 +69,7 @@ export function useRenderRadioOrCheckbox(
                 class: `${cssPrefix}${inputType}-inner`,
                 onClick: () => toggleCheckHandler(),
             }, [
-                h("div", {class: `${cssPrefix}field-overlay`}),
+                h("div", {class: `${cssPrefix}${inputType}-overlay`}),
                 h<TBsRipple>(BsRipple, {
                     centered: true as Prop<boolean>,
                     active: <Prop<boolean>>rippleActive.value,
