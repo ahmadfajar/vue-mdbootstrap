fb1cda722feedcd8173ca9e63b409c1d0c110425
Ahmad Fajar <ahmad.fajar234@hotmail.com>
2023-12-22 01:32:04 +0700

    Improve BsImageHolder component

--- src/components/Basic/BsImageHolder.ts
+++ src/components/Basic/BsImageHolder.ts
@@ -1,5 +1,5 @@
 import type { ComponentOptionsMixin, ComputedOptions, EmitsOptions, MethodOptions } from 'vue';
-import { defineComponent, h, toDisplayString } from 'vue';
+import { computed, defineComponent, h, toDisplayString } from 'vue';
 import { cssPrefix } from '../../mixins/CommonApi';
 import type { TBsImageHolder, TImageHolderOptionProps, TRecord } from '../../types';
 import Helper from '../../utils/Helper';
@@ -13,9 +13,9 @@ export default defineComponent<TBsImageHolder, TRecord, TRecord, ComputedOptions
     props: imageHolderProps,
     setup(props) {
         const thisProps = props as Readonly<TImageHolderOptionProps>;
-        const showText = () => {
+        const showText = computed(() => {
             return !Helper.isEmpty(props.placeholderText) || !Helper.isEmpty(props.placeHolder);
-        };
+        });
         const szHeight = useSizeHeight(thisProps);
         const szWidth = useSizeWidth(thisProps);
 
@@ -29,11 +29,11 @@ export default defineComponent<TBsImageHolder, TRecord, TRecord, ComputedOptions
                 width: !szWidth || (<number>szWidth < 2) ? '100%' : Helper.cssUnit(szWidth),
                 role: 'img',
             }, [
-                showText()
+                showText.value
                     ? h('title', toDisplayString(thisProps.placeHolder || thisProps.placeholderText))
                     : undefined,
                 h('rect', {width: '100%', height: '100%', fill: thisProps.bgColor}),
-                showText()
+                showText.value
                     ? h('text', {
                             fill: thisProps.textColor,
                             x: Helper.cssUnit(thisProps.xPos),
